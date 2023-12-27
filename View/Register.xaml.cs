82b0cd08b9a0ddc49961c0ba6307c8d48f79db14
Dave-Birnie <102973173+Dave-Birnie@users.noreply.github.com>
2023-12-27 12:33:41 -0500

    Added Register and UserLogin pages and startup

---
+++ View/Register.xaml.cs
@@ -0,0 +1,117 @@
+﻿using BingoFlashboard.Model;
+using Microsoft.AspNetCore.SignalR.Client;
+using System;
+using System.Windows;
+using System.Windows.Input;
+using System.Windows.Media;
+
+namespace BingoFlashboard.View
+{
+    public partial class Register : Window
+    {
+        HubConnection? connection;
+        Hall? hall;
+
+        public Register()
+        {
+            InitializeComponent();
+
+            connection = new HubConnectionBuilder()
+            .WithUrl("https://bingoappservice.azurewebsites.net/RegisterHub")
+            //.WithUrl("https://localhost:7226/RegisterHub")
+            .WithAutomaticReconnect()
+            .Build();
+
+            ConnectToServer();
+        }
+
+        private void ConnectToServer()
+        {
+            if (connection != null)
+            {
+                MessageLbl.Text = "";
+
+               _ = connection.On<string>("RegisterReply", (message) =>
+                {
+                    this.Dispatcher.Invoke(() =>
+                    {
+                        if (message== "Incorrect Credentials")
+                        {
+                            MessageLbl.Text = "Incorrect Username or Password\nPlease try again";
+                            MessageLbl.Foreground = new SolidColorBrush(Colors.Red);
+                        }
+                        else if (message == "Success")
+                        {
+                            MessageLbl.Text = "Password Updated";
+                            MessageLbl.Foreground = new SolidColorBrush(Colors.Green);
+                            System.Threading.Thread.Sleep(2500);
+                            UserLogin userWindow = new UserLogin();
+                            userWindow.Show();
+                            this.Close();
+                        }
+                        else
+                        {
+                            MessageLbl.Text = message;
+                            MessageLbl.Foreground = new SolidColorBrush(Colors.Red);
+                        }
+                    });
+                });
+            }
+        }
+
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                DragMove();
+            }
+        }
+        private async void SubmitBtn_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                if(NewPassword.Password == ConfirmPassword.Password)
+                {
+
+
+                MessageLbl.Text = "Updating Password";
+                MessageLbl.Foreground = new SolidColorBrush(Colors.Yellow);
+
+                //int? password = NewPassword.Password.GetHashCode();
+
+                if (NewPassword.Password == ConfirmPassword.Password && connection != null)
+                {
+                    //byte[] salt = RandomNumberGenerator.GetBytes(128 / 8); // divide by 8 to convert bits to bytes
+                    //string hashed = Convert.ToBase64String(KeyDerivation.Pbkdf2(
+                    //    password: password!,
+                    //    salt: salt,
+                    //    prf: KeyDerivationPrf.HMACSHA256,
+                    //    iterationCount: 100000,
+                    //    numBytesRequested: 256 / 8));
+
+                    await connection.StartAsync();
+                    await connection.InvokeAsync("RegisterMessage", Username.Text, NewPassword.Password, TempPassword.Password);
+                }
+                }
+                else
+                {
+                    MessageLbl.Text = "Passwords not matching";
+                    MessageLbl.Foreground = new SolidColorBrush(Colors.Red);
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageLbl.Text = "Error: " + ex.Message;
+            }
+        }
+
+        #region EXIT APP
+
+        private void ExitApp_Click(object sender, RoutedEventArgs e)
+        {
+            App.Current.Shutdown();
+        }
+
+        #endregion
+    }
+}
---
+++ View/UserLogin.xaml.cs
@@ -0,0 +1,173 @@
+﻿using BingoFlashboard.Data;
+using BingoFlashboard.Model;
+using Microsoft.AspNetCore.SignalR.Client;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace BingoFlashboard.View
+{
+    /// <summary>
+    /// Interaction logic for UserLogin.xaml
+    /// </summary>
+    public partial class UserLogin : Window
+    {
+        HubConnection connection;
+
+        public UserLogin()
+        {
+            InitializeComponent();
+
+            connection = new HubConnectionBuilder()
+            .WithUrl("https://bingoappservice.azurewebsites.net/LoginHub")
+            //.WithUrl("https://localhost:7226/LoginHub")
+
+            .WithAutomaticReconnect()
+            .Build();
+            ConnectToServer();
+            Startup();
+
+        }
+
+        private void Startup()
+        {
+            if (App.startupFile != null)
+            {
+                if (File.Exists(App.startupFile))
+                {
+                    string jsonTxt = File.ReadAllText(App.startupFile);
+                    StartupClass? sc = JsonConvert.DeserializeObject<StartupClass>(jsonTxt);
+
+                    if (sc != null)
+                    {
+                        Username.Text = sc.UserName;
+                        UserPassword.Password = sc.Password;
+                        App.startup = sc;
+                        CheckboxRemember.IsChecked = true;
+                    }
+                }
+            }
+        }
+
+        private void ConnectToServer()
+        {
+            if (connection != null)
+            {
+                _ = connection.On<string, string>("LoginReply", (credentials, jsonHallInfo) =>
+                {
+                    this.Dispatcher.Invoke(() =>
+                    {
+
+                        Hall? bh = JsonConvert.DeserializeObject<Hall>(jsonHallInfo);
+
+                        if (bh != null)
+                        {
+                            App.hall = bh;
+                        }
+
+                        if (credentials == "Success")
+                        {
+                            MessageLbl.Text = "Success";
+                            MessageLbl.Foreground = new SolidColorBrush(Colors.Green);
+
+                            if (CheckboxRemember.IsChecked != null)
+                            {
+                                if ((bool) CheckboxRemember.IsChecked)
+                                {
+                                    App.startup.UserName = Username.Text;
+                                    App.startup.Password = UserPassword.Password;
+                                    App.SaveStartupFile();
+                                }
+                            }
+
+                            //TODO - Get and update startupFile
+
+                            System.Threading.Thread.Sleep(1500);
+                            App.startupWindow = new StartupWindow();
+                            App.startupWindow.Show();
+                            this.Close();
+                        }
+                        else if (credentials == "Success - Temp Password")
+                        {
+                            Register registerWindow = new Register();
+                            registerWindow.Username.Text = Username.Text;
+                            registerWindow.TempPassword.Password = UserPassword.Password;
+                            registerWindow.Show();
+                            this.Hide();
+                        }
+                        else if (credentials == "Incorrect Credentials")
+                        {
+                            MessageLbl.Text = "Incorrect Username or Password\nPlease try again";
+                            MessageLbl.Foreground = new SolidColorBrush(Colors.Yellow);
+                        }
+                        else
+                        {
+                            MessageLbl.Text = credentials;
+                            MessageLbl.Foreground = new SolidColorBrush(Colors.Yellow);
+                        }
+                    });
+                });
+            }
+        }
+
+        private async void Login_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                if (UserPassword.Password != "" && Username.Text != "")
+                {
+                    Login.IsEnabled = false;
+                    MessageLbl.Text = "Signing In";
+                    MessageLbl.Foreground = new SolidColorBrush(Colors.Yellow);
+                    if (connection.State == HubConnectionState.Disconnected)
+                        await connection.StartAsync();
+                        await connection.InvokeAsync("LoginMessage", Username.Text, UserPassword.Password);
+                }
+                else
+                {
+                    MessageLbl.Text = "Please enter both Username and Password";
+                    MessageLbl.Foreground = new SolidColorBrush(Colors.Red);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageLbl.Text = "Error: " + ex.Message;
+            }
+        }
+
+        private void Register_Click(object sender, RoutedEventArgs e)
+        {
+            Register registerWindow = new Register();
+            registerWindow.Show();
+            this.Hide();
+        }
+
+        #region Window_MouseDown and ExitApp_Click
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                DragMove();
+            }
+        }
+
+        private void ExitApp_Click(object sender, RoutedEventArgs e)
+        {
+            App.Current.Shutdown();
+        }
+        #endregion
+
+    }
+}
